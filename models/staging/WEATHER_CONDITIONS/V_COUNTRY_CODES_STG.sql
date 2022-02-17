f5f3108af48d89246aa6a6115473d2a52739ce49
guilhermealcant <galcantara@fullstacklabs.co>
2022-02-17 08:46:01 -0300

    Add Weather data

---
+++ models/staging/WEATHER_CONDITIONS/V_COUNTRY_CODES_STG.sql
@@ -0,0 +1,10 @@
+ {{ config(
+    materialized='view'    
+) }}
+
+SELECT    
+    COUNTRY AS A_COUNTRY_NAME,
+    ALPHA2_CODE AS A_ALPHA2_COUNTRY_CODE,
+    ALPHA3_CODE AS A_ALPHA3_COUNTRY_CODE
+FROM
+    {{source('TEST_SCHEMA_EXT_DEV','CONTRY_CODES')}}
