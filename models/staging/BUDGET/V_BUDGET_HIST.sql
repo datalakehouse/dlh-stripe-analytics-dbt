d0831289707415b4977355ed12d91c8f7cc134db
Guilherme Alcantara <guilhermealc@hotmail.com>
2022-02-22 09:33:30 -0300

    Naming conventions update

--- models/staging/BUDGET/V_BUDGET_HIST.sql
+++ models/staging/BUDGET/V_BUDGET_HIST.sql
@@ -21,6 +21,7 @@ SELECT DISTINCT
   ,H.START_DATE
   ,H.END_DATE
   , '{{invocation_id}}' as MD_INTGR_ID
+  , CURRENT_TIMESTAMP() MD_ELT_UPDATED_DTS
 FROM 
   budget_orders O
   INNER JOIN budget_hist H ON H.BUDGET_ID = O.K_BUDGET_BK
